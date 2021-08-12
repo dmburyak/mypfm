23945bbef7c894ea75799843213a2e2997e27057
Dmitri <d.buryak@gmail.com>
2021-08-12 14:32:33 +0300

    create project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { StatisticPageComponent } from './home-page/statistic-page/statistic-page.component';
+import { ComparePageComponent } from './home-page/compare-page/compare-page.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BarChartComponent } from './home-page/compare-page/bar-chart/bar-chart.component';
+import { PieChartComponent } from './home-page/statistic-page/pie-chart/pie-chart.component';
+import { TableComponent } from './home-page/statistic-page/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent,
+    StatisticPageComponent,
+    ComparePageComponent,
+    HeaderComponent,
+    BarChartComponent,
+    PieChartComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
