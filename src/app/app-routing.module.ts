869fe60adc8e17f223eb85ef0ecf703dedbd152d
Dmitri <d.buryak@gmail.com>
2021-08-12 17:09:14 +0300

    add routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ComparePageComponent } from './home-page/compare-page/compare-page.component';
+import { StatisticPageComponent } from './home-page/statistic-page/statistic-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {
+    path: '', component: HomePageComponent,
+    children: [
+      {path: 'compare', component: ComparePageComponent},
+      {path: '', component: StatisticPageComponent},
+    ]
+  },
+  {path: '**', redirectTo: ''},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
